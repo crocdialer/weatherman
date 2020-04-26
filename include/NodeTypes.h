8d3cda9070440c8021bed718580c56a3145a6ec9
Croc Dialer <crocdialer@gmail.com>
2020-04-26 15:37:20 +0200

    nice first version

--- include/NodeTypes.h
+++ include/NodeTypes.h
@@ -8,7 +8,18 @@
 typedef struct weather_t
 {
     uint8_t stype = STRUCT_TYPE_WEATHERMAN;
+
     uint8_t battery = 0;
+
+    //! temperature in range [-100C .. 100C]
+    uint16_t temperature = 0;
+
+    //! pressure in range [0hPa .. 2000hPa]
+    uint16_t pressure = 0;
+
+    // relative humidity in range [0..1]
+    uint8_t humidity = 0;
+
 } weather_t;
 
 #endif
--- src/weatherman.cpp
+++ src/weatherman.cpp
@@ -6,6 +6,10 @@
 #include "rfm95_config.h"
 #include "NodeTypes.h"
 
+// BME temperature/pressure/humidity sensor
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
 // analog-digital-converter (ADC)
 #define ADC_BITS 10
 constexpr uint32_t ADC_MAX = (1 << ADC_BITS) - 1U;
@@ -21,7 +25,8 @@ long g_last_time_stamp;
 enum TimerEnum
 {
   TIMER_LORA_SEND = 0,
-  TIMER_BATTERY_MEASURE = 2,
+  TIMER_BATTERY_MEASURE = 1,
+  TIMER_SENSOR_MEASURE = 2,
   NUM_TIMERS
 };
 kinski::Timer g_timer[NUM_TIMERS];
@@ -29,6 +34,18 @@ kinski::Timer g_timer[NUM_TIMERS];
 // battery
 constexpr uint8_t g_battery_pin = A7;
 uint8_t g_battery_val = 0;
+
+// BME280 temperature/pressure/humidity sensor
+Adafruit_BME280 g_sensor;
+
+// last measurements
+float g_temperature;
+
+// in hPa
+float g_pressure;
+
+// relative in range [0..1]
+float g_humidity;
 
 ////////////////////////////////////////////////////////////////////////////////
 // forward declared functions
@@ -48,7 +65,9 @@ lora::driver_struct_t m_rfm95 = {};
 //! lora message buffer
 uint8_t g_lora_buffer[RH_RF95_MAX_MESSAGE_LEN];
 
-float g_lora_send_interval = .2f;
+float g_lora_send_interval = 2.f;
+
+////////////////////////////////////////////////////////////////////////////////
 
 void set_address(uint8_t address)
 {
@@ -63,6 +82,7 @@ void set_address(uint8_t address)
     else{ Serial.println("LoRa radio init failed"); }
 }
 
+////////////////////////////////////////////////////////////////////////////////
 void lora_receive()
 {
     uint8_t len = sizeof(g_lora_buffer);
@@ -75,6 +95,8 @@ void lora_receive()
     }
 }
 
+////////////////////////////////////////////////////////////////////////////////
+
 bool lora_send_status()
 {
     bool ret = false;
@@ -85,6 +107,9 @@ bool lora_send_status()
     weather_t &weather = *(weather_t*)data;
     weather = {};
     weather.battery = g_battery_val;
+    weather.temperature = map_value<float>(g_temperature, -100.f, 100.f, 0, 65536);
+    weather.pressure = map_value<float>(g_pressure, 0.f, 2000.f, 0, 65536);
+    weather.humidity = g_humidity * 255;
 
     // checksum
     data[num_bytes - 1] = crc8(data, sizeof(weather_t));
@@ -94,11 +119,6 @@ bool lora_send_status()
     {
         // the message has been reliably delivered to the next node.
         ret = true;
-    }
-    else
-    {
-        // failed to send to next hop
-        Serial.println("lora: could not find a route to destination ...");
     }
     return ret;
 }
@@ -145,6 +165,45 @@ void setup()
     g_timer[TIMER_LORA_SEND].set_callback([](){ lora_send_status(); });
     g_timer[TIMER_LORA_SEND].set_periodic();
     g_timer[TIMER_LORA_SEND].expires_from_now(g_lora_send_interval);
+
+    // BME setup
+    if(!g_sensor.begin())
+    {
+        Serial.println("Could not find a valid BME280 sensor, check wiring!");
+        while(true){ blink_status_led(); }
+    }
+
+    // sensor measuring
+    g_timer[TIMER_SENSOR_MEASURE].set_callback([]()
+    {
+        g_sensor.takeForcedMeasurement();
+
+        g_temperature = g_sensor.readTemperature();
+        g_pressure = g_sensor.readPressure() / 100.f;
+        g_humidity = g_sensor.readHumidity() / 100.f;
+
+        Serial.print("Temperature = ");
+        Serial.print(g_sensor.readTemperature());
+        Serial.println(" *C");
+
+        Serial.print("Pressure = ");
+
+        Serial.print(g_sensor.readPressure() / 100.0F);
+        Serial.println(" hPa");
+
+        // Serial.print("Approx. Altitude = ");
+        // Serial.print(g_sensor.readAltitude(SEALEVELPRESSURE_HPA));
+        // Serial.println(" m");
+
+        Serial.print("Humidity = ");
+        Serial.print(g_sensor.readHumidity());
+        Serial.println(" %");
+
+        Serial.println();
+
+      });
+      g_timer[TIMER_SENSOR_MEASURE].set_periodic();
+      g_timer[TIMER_SENSOR_MEASURE].expires_from_now(.4f);
 
     digitalWrite(13, LOW);
 }
